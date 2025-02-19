ac44ad1546313af335f1081a9887555918e50796
Lutfor Rahman <lutforr3845@gmail.com>
2025-02-19 14:34:27 +0600

    add testimonial reviews

---
+++ src/Layout/Components/Reviews.jsx
@@ -0,0 +1,103 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import Loading from "./Loading";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import userIcon from "../../assets/photo.png";
+import ReactStars from "react-stars";
+
+const Reviews = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: reviews, isLoading } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/testimonial-reviews`);
+      return data;
+    },
+  });
+  if (isLoading) return <Loading></Loading>;
+  return (
+    <div className="py-8 md:py-14 lg:py-16  px-2 overflow-x-hidden font-roboto">
+      <h1 className="text-xl  lg:text-2xl  xl:text-3xl text-center font-poppins font-semibold">
+        Testimonial
+      </h1>
+      <p className="text-center text-gray-600 mb-6 max-w-4xl mx-auto">
+        
+      </p>
+      <Swiper
+        spaceBetween={30}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        className="w-full h-72 my-5 mx-10 mt-8"
+      >
+        {reviews.slice(0, 8).map((review) => (
+          <SwiperSlide
+            key={review._id}
+            className="border-2 border-indigo-400 p-4 rounded-lg "
+          >
+            {" "}
+            <div>
+              <div className="flex items-center gap-2">
+                {review?.reviewerPhoto ? (
+                  <img
+                    className="w-12 h-12 object-cover rounded-full"
+                    src={review?.reviewerPhoto}
+                    alt="User imge"
+                    referrerPolicy="no-referrer"
+                  />
+                ) : (
+                  <img
+                    className="w-12 h-12 rounded-full border border-primary p-[2px]"
+                    src={userIcon}
+                    alt="Default User Icon"
+                  />
+                )}
+                <div>
+                  <p className="text-lg font-semibold">{review?.reviewerName}</p>
+                  <p>{new Date(review?.date).toLocaleString()}</p>
+                </div>
+              </div>
+              <div className="my-4 ml-10">
+                <div className="font-medium flex items-center gap-2">
+                  Rating :{" "}
+                  <ReactStars
+                    count={5}
+                    size={24}
+                    color2={"#ffd700"}
+                    half={true}
+                    value={review?.rating}
+                    edit={false}
+                  />
+                </div>
+                <p className="font-medium max-w-3xl">
+                  Comment :{" "}
+                  <span className="text-gray-700 dark:text-gray-400 font-normal">
+                    {" "}
+                    {review?.comment}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Reviews;
